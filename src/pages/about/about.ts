62ef9973b190cb45491a56d61c450ed90d3b8d78
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-01-29 18:18:22 +0300

    feat: add about page

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,11 +1,54 @@
+import AnchorCreator from "../../utils/anchor/anchor-creator.js";
+import ButtonCreator from "../../utils/button/button-creator.js";
+import ElementCreator from "../../utils/element-creator.js";
+import HeadingsCreator from "../../utils/headings/headings-creator.js";
+import ParagraphCreator from "../../utils/paragraph/paragraph-creator.js";
 import { BasePage } from "../base-page.js";
+import "./about.css";
+
+const HEADINGS_TWO = 2;
 
 export class About extends BasePage {
   create(parent: HTMLElement): void {
     parent.append(this.container);
-    this.container.innerHTML = `
-      <h2>About</h2>
-      <button data-back>Go Back</button>
-    `;
+    this.container.classList.add("about-page");
+    const aboutInfo = new ElementCreator({
+      classes: ["about-info"],
+      parent: this.container,
+    }).getElement();
+
+    const appTitle = new HeadingsCreator(HEADINGS_TWO, {
+      parent: aboutInfo,
+      classes: ["about-info__app-title"],
+    }).getElement();
+    appTitle.textContent = "Fun Chat";
+
+    const aboutDescription = new ParagraphCreator({
+      parent: aboutInfo,
+      classes: ["about-description"],
+    }).getElement();
+    aboutDescription.textContent =
+      "The application was developed during the training process within the RSSchool JS/FE 2025Q3 course.";
+
+    const aboutAuthor = new ParagraphCreator({
+      parent: aboutInfo,
+      classes: ["about-author"],
+      text: "Author: ",
+    }).getElement();
+
+    const githubLink = new AnchorCreator({
+      parent: aboutAuthor,
+      href: "https://github.com/Atikin132",
+      target: "_blank",
+    }).getElement();
+    githubLink.classList.add("about-info__link");
+    githubLink.textContent = "Atikin132";
+
+    const backButton = new ButtonCreator({
+      text: "Back",
+      classes: ["about-info__back-button", "button"],
+      parent: aboutInfo,
+    }).getElement();
+    backButton.dataset.back = "";
   }
 }
