643c169678460c467c225d7f7260f5ee6b6b4863
Atikin132 <nikita.kupriyan0v@yahoo.com>
2026-07-31 11:36:06 +0300

    feat: add gitignore and README

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/atikin132-JSFE2025Q3/fun-chat/",
+  base: "/fun-chat/",
   server: {
     port: 8080,
     open: true,
